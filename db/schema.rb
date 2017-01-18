d286ce57c8bf47d277aa6ba38ccacde59695ef89
Anthony Mattox <anthony@friendsoftheweb.com>
2017-01-18 11:37:02 -0500

    Adds Curds
    Signed-off-by: Anthony Mattox <anthony@friendsoftheweb.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117214742) do
+ActiveRecord::Schema.define(version: 20170118163638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "curds", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "lemons", force: :cascade do |t|
     t.string  "nickname"
